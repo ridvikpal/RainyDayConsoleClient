60ebf8cd916da6cf50c3cf2c40991f88331b6c4b
Ridvik Pal <pal.ridvik@gmail.com>
2023-07-16 00:35:53 -0400

    Moved subclasses to their own classes. Am thinking about making WeatherClass an interface and creating seperate classes for each type of API call.

---
+++ src/main/java/org/rainyday/ForecastDay.java
@@ -0,0 +1,11 @@
+package org.rainyday;
+
+import java.util.ArrayList;
+
+public class ForecastDay {
+    private String date;
+    private int date_epoch;
+    private Astro astro;
+    private Day day;
+    ArrayList<Hour> hour;
+}
--- src/main/java/org/rainyday/Main.java
+++ src/main/java/org/rainyday/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Connection connectionObject = new Connection();
 
 //        Weather test = connectionObject.getCurrentWeather("Jacksonville", "yes");
-//        Weather test = connectionObject.getAstronomy("Toronto", "2023-07-13");
-        Weather test = connectionObject.getForecast("Toronto", 1, "no", "yes");
+        Weather test = connectionObject.getAstronomy("Toronto", "2023-07-13");
+//        Weather test = connectionObject.getForecast("Toronto", 1, "yes", "yes");
 
         System.out.println("Done");
     }
